1b957e65c06586d2ded87c3e19769bc01fe9d992
Ivan Achille <ivanachillee@gmail.com>
2021-05-13 00:20:15 -0300

    Added missing files

---
+++ serverless-aws-lambda/scikit_learn_lambda/s3_url.py
@@ -0,0 +1,25 @@
+from urllib.parse import urlparse
+
+
+class S3Url(object):
+    def __init__(self, url):
+        self._parsed = urlparse(url, allow_fragments=False)
+
+    @property
+    def bucket(self):
+        return self._parsed.netloc
+
+    @property
+    def key(self):
+        if self._parsed.query:
+            return self._parsed.path.lstrip("/") + "?" + self._parsed.query
+        else:
+            return self._parsed.path.lstrip("/")
+
+    @property
+    def filename(self):
+        return self._parsed.path.split("/")[-1]
+
+    @property
+    def url(self):
+        return self._parsed.geturl()
